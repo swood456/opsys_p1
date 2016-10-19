7b0c1857af94d701c01cd5ee53c7a68c6c9982f7
stephen <crazedsportsfan@gmail.com>
2016-10-19 10:06:13 -0400

    added input files, some small amount of file I/O and input checking

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+
+
+
+int main(int argc, char* argv[]){
+
+	//check arguments
+	if(argc != 3){
+		fprintf(stderr, "ERROR: Invalid arguments\nUSAGE: ./a.out <input-file> <stats-output-file>\n" );
+		return EXIT_FAILURE;
+	}
+
+	//open argv[1] to read file
+	std::ifstream inFile;
+	inFile.open(argv[1]);
+
+	//check to see if the file is able to be opened
+	if(!inFile){
+		fprintf(stderr, "ERROR: Invalid input file format\n" );
+		return EXIT_FAILURE;
+	}
+
+	inFile.close();
+
+
+
+	
+	std::cout << "hello world" << std::endl;
+
+
+
+	return EXIT_SUCCESS;
+}
 
 class Process{
+
 private:
 	int processID;
 	int initialArrivalTime;
@@ -8,7 +44,8 @@ private:
 	int totalNumBursts;
 	int ioTime;
 
-	public Process(int _processID, int _initialArrivalTime, int _totalCpuBurstTime int _totalNumBursts, int _ioTime){
+public:
+	Process(int _processID, int _initialArrivalTime, int _totalCpuBurstTime, int _totalNumBursts, int _ioTime){
 		processID = _processID;
 		initialArrivalTime = _initialArrivalTime;
 		totalCpuBurstTime = _totalCpuBurstTime;
@@ -16,12 +53,3 @@ private:
 		ioTime = _ioTime;
 	}
 };
-
-int main(int argc, char* argv[]){
-	
-	std::cout << "hello world" << std::endl;
-
-
-
-	return true;
-}
