3052aaaad987664113e157139f523796e6d402f6
stephen <crazedsportsfan@gmail.com>
2016-10-19 13:58:44 -0400

    fcfs bad implementation done, I do not think this is the best way to do things

--- main.cpp
+++ main.cpp
@@ -4,17 +4,25 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <sstream>
+#include <queue>
+
+const int m = 1;		//number of processors available
+const int t_cs = 8;		//time it takes to complete a context switch
+const int t_slice = 84;	//time slice value
 
 
 class Process{
-
-private:
+//these should be made private eventually, but they never will be
+public:
 	std::string processID;
 	int initialArrivalTime;
 	int totalCpuBurstTime;
 	int totalNumBursts;
 	int ioTime;
+
+	int curNumBursts;
+	int curIOTime;
+	int curProcessTime;
 
 public:
 	Process(std::string _processID, int _initialArrivalTime, int _totalCpuBurstTime, int _totalNumBursts, int _ioTime){
@@ -23,10 +31,44 @@ public:
 		totalCpuBurstTime = _totalCpuBurstTime;
 		totalNumBursts = _totalNumBursts;
 		ioTime = _ioTime;
+
+		curNumBursts = totalNumBursts;
+		curIOTime = ioTime;
+		curProcessTime = totalCpuBurstTime;
 	}
 
 	void print(){
 		std::cout << processID << " " << initialArrivalTime << " " << totalCpuBurstTime << " " << totalNumBursts << " " << ioTime << std::endl;
+	}
+
+	int run(int duration){
+		curProcessTime -= duration;
+
+		if(curProcessTime <= 0){
+			curNumBursts--;
+			curProcessTime = totalCpuBurstTime;
+			return 1;
+		}
+		return 0;
+	}
+
+	int runIO(int duration){
+		curIOTime -= duration;
+
+		if(curIOTime <= 0){
+			curIOTime = ioTime;
+			return 1;
+		}
+		return 0;
+	}
+};
+
+struct FirstComeLessThan{
+	bool operator()(const Process& lhs, const Process& rhs) const{
+		if(lhs.initialArrivalTime == rhs.initialArrivalTime){
+			return lhs.processID > rhs.processID;
+		}
+		return lhs.initialArrivalTime > rhs.initialArrivalTime;
 	}
 };
 
@@ -98,7 +140,78 @@ int main(int argc, char* argv[]){
 		processes[i].print();
 	}
 
-	//do First-Come-First-Serve
+	//////////////////////////
+	//First-Come-First-Serve//
+	//////////////////////////
+
+	std::vector<Process> fcfsAdding(processes);
+
+	//sort the vector by process ID?
+	std::queue<Process> fcfsQueue;
+	std::queue<Process> fcfsIOQueue;
+
+	//This is not a smart way to do things
+	std::vector<Process>::iterator addingItr;
+	int time = 0;
+	while(!fcfsAdding.empty() || !fcfsQueue.empty() || !fcfsIOQueue.empty()){
+		//std::cout <<"hello\n";
+		if(!fcfsIOQueue.empty()){
+			if(fcfsIOQueue.front().runIO(1) == 1){
+				std::cout << "finished IO on process with ID " << fcfsIOQueue.front().processID << " and pop'd it at time: " << time << std::endl;
+				if(fcfsIOQueue.front().curNumBursts > 0){
+					fcfsQueue.push(fcfsIOQueue.front());
+				}
+				fcfsIOQueue.pop();
+			}
+		}
+
+
+		//run the first thing is the processing queue
+		if(!fcfsQueue.empty()){
+			if(fcfsQueue.front().run(1) == 1){
+				std::cout << "finished running process with ID " << fcfsQueue.front().processID << " and pop'd it at time: " << time << std::endl;
+				fcfsIOQueue.push(fcfsQueue.front());
+
+				fcfsQueue.pop();
+			}
+		}
+
+		//look to see if any processes arrive
+		addingItr = fcfsAdding.begin();
+		while(addingItr != fcfsAdding.end()){
+
+			if(addingItr->initialArrivalTime == time){
+			
+				fcfsQueue.push(*addingItr);
+				fcfsAdding.erase(addingItr);
+
+				std::cout << "added a thing\n";
+			
+			} else{
+				addingItr++;
+			}
+		}
+
+		
+
+		time++;
+	}
+	/*
+	std::priority_queue<Process, std::vector<Process>, FirstComeLessThan> fcfsPriorityQueue;
+
+	//for(int i = 0; i < processes)
+	for(unsigned int i = 0; i < processes.size(); i++){
+		//processes[i].print();
+		fcfsPriorityQueue.push(processes[i]);
+	}
+
+	std::cout << "now in fcfs order\n";
+	while(!fcfsPriorityQueue.empty()){
+		Process temp = fcfsPriorityQueue.top();
+		//fcfsPriorityQueue.top().print();
+		temp.print();
+		fcfsPriorityQueue.pop();
+	}*/
 
 	//do Shortest Job First
 
